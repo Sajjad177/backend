492a5312a6afd13c10a0b0ae9ed3ecbe68b7e67f
sajjad <sajjadsajjad098765@gmail.com>
2026-04-03 18:43:59 +0600

    error fixed

--- src/modules/notification/notification.interface.ts
+++
@@ -1,9 +0,0 @@
-import mongoose, { Document } from "mongoose";
-
-export interface INotification extends Document {
-  to: mongoose.Types.ObjectId;
-  message: string;
-  isViewed: boolean;
-  type: string;
-  id: mongoose.Types.ObjectId;
-}
